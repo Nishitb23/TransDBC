d66aed0b016c8a85886a6fb40796d95bcab7b512
Nishit Bhardwaj <nishitb23@gmail.com>
2022-05-21 16:34:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from torch.utils.data import TensorDataset
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.metrics import accuracy_score
+
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
