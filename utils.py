042ba549c803145a1bfcee07592208d0d4a68b6d
Nishit Bhardwaj <nishitb23@gmail.com>
2022-05-21 16:17:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -24,3 +24,39 @@ def load_full_dataset(data_path='drive/MyDrive/data'):
     X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=0.20, random_state=42)
 
     return X_train, X_test, y_train, y_test
+
+#model training function
+def train(model,n_epochs,trainloader,optimizer,lossfn,test_dataloader,onehotencoding=True,n_class=3):
+  predlist=torch.zeros(0,dtype=torch.long, device='cpu')
+  lbllist=torch.zeros(0,dtype=torch.long, device='cpu')
+  for epoch in range(n_epochs):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+      # get the inputs; data is a list of [inputs, labels]
+      inputs, labels = data   
+      # zero the parameter gradients
+      optimizer.zero_grad()
+      # forward + backward + optimize
+      #inputs = inputs.view(-1,1,64,9)
+      outputs = model(inputs)
+      #print(outputs.shape)
+      if onehotencoding == True:
+        #new_labels = torch.tensor(onehotvector[labels])
+        #print(labels.shape)
+        new_labels = torch.nn.functional.one_hot(labels.to(torch.int64),n_class).to(torch.float32)
+      else:
+        new_labels = labels
+      loss = lossfn(outputs, new_labels)
+
+      loss.backward()
+      optimizer.step()
+      # print statistics
+      running_loss += loss.item()
+      predlist=torch.cat([predlist,torch.argmax(outputs, dim=1).view(-1).cpu()])
+      lbllist=torch.cat([lbllist,labels.view(-1).cpu()])
+    print()
+    print("The training loss for epoch ",epoch+1," is: ",loss)
+    # Confusion matrix
+    
+    print("the training accuracy acheived is: ",accuracy_score(lbllist,predlist))
+    test(model,test_dataloader,lossfn)
