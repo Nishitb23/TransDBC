a72b9de653fbb99bf7a1d1eaeb55ad0a44a9b5ac
Nishit Bhardwaj <nishitb23@gmail.com>
2022-05-23 12:48:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from utils import *
 from model import *
+from config import *
 
 
 
@@ -16,6 +17,7 @@ print(device)
 X_train, X_test, y_train, y_test = load_full_dataset()
 print('Training dataset shape: ', X_train.shape, y_train.shape)
 print('Testing dataset shape: ', X_test.shape, y_test.shape)
+params = Params(X_train)
 
 #converting data into tensor form and creating dataloader
 X_train = torch.Tensor(X_train).to(device)
@@ -26,12 +28,12 @@ y_test = torch.Tensor(y_test).reshape(-1).to(device)
 train_dataset = TensorDataset(X_train,  y_train)
 test_dataset = TensorDataset(X_test,  y_test)
 
-train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+train_dataloader = DataLoader(dataset=train_dataset, batch_size=params.batch_size, shuffle=True)
+test_dataloader = DataLoader(dataset=test_dataset, batch_size=params.batch_size, shuffle=False)
 
 lossfn = nn.CrossEntropyLoss()
 
-model = Net(input_size,ff_dim,n_head,n_classes,n_layers,dropout)
+model = Net(params.n_channels,params.time_steps, params.ff_dim,params.n_head,params.n_classes,params.n_layers,params.dropout)
 model.to(device)
 optimizer= optim.Adam(model.parameters(), lr= learning_rate)
 
